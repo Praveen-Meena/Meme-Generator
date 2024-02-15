ad0a84772b13cb3facd47cf613f29a7d62fe64bf
Praveen-Meena <singhjimeena123@gmail.com>
2024-02-15 12:19:53 +0530

    Meme Generator

---
+++ src/components/Random.jsx
@@ -0,0 +1,50 @@
+import React, {useEffect, useState} from "react";
+import Spinner from "./Spinner";
+import axios from "axios";
+import useGif from "../hooks/useGif";
+
+// const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+
+const Random = () => {
+
+  // const [gif, setGif] = useState(''); 
+  // const [loader, setLoader] = useState('false'); 
+ 
+  
+  // async function fetchData(){
+  //   setLoader(true); 
+  //   const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+  //   const {data} = await axios.get(url); 
+  //   const imageSource = data.data.images.downsized_large.url;
+  //   setGif(imageSource); 
+  //   setLoader(false); 
+  // }
+
+  // useEffect( 
+  //   ()=>{
+  //     fetchData(); 
+  //   },
+  //   []
+  // )
+
+  const {gif, loader, fetchData} = useGif();   //using custom Hook for code reusability
+  
+  return (
+    <div className="w-1/2 bg-green-500 rounded-lg border border-black 
+                    flex flex-col items-center gap-y-5">
+      <h1 className=" mt-[15px] text-2xl font-bold underline uppercase">A Random Gif</h1>
+
+      {
+        loader?(<Spinner/>):(<img src={gif} width="450" />)  
+      } 
+
+      <button onClick={() => fetchData()} 
+              className="w-10/12 bg-blue-200  text-lg py-2 rounded-lg mb-[20px]">
+        Generate
+      </button>
+
+    </div>
+  );  
+}
+
+export default  Random; 
---
+++ src/components/Tag.jsx
@@ -0,0 +1,58 @@
+import React, {useEffect, useState} from "react";
+import Spinner from "./Spinner";
+import axios from "axios";
+import useGif from "../hooks/useGif";
+
+
+// const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+
+const Tag = () => {
+
+  const [tag, setTag] = useState(''); 
+  // const [gif, setGif] = useState(''); 
+  // const [loader, setLoader] = useState('false'); 
+ 
+  
+  // async function fetchData(){
+  //   setLoader(true); 
+  //   const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
+  //   const {data} = await axios.get(url); 
+  //   const imageSource = data.data.images.downsized_large.url;
+  //   setGif(imageSource); 
+  //   setLoader(false); 
+  // }
+
+  // useEffect( 
+  //   ()=>{
+  //     fetchData(); 
+  //   },
+  //   []
+  // )
+
+  const {gif, loader, fetchData} = useGif(tag); //Using Custom Hook useGif for code reusability
+
+  return (
+    <div className="w-1/2 bg-blue-500 rounded-lg border border-black 
+                    flex flex-col items-center gap-y-5">
+      <h1 className=" mt-[15px] text-2xl font-bold underline uppercase">Gif for You</h1>
+
+      {
+        loader?(<Spinner/>):(<img src={gif} width="450" />)  
+      } 
+
+      <input
+       className="w-10/12 bg-white text-lg py-2 rounded-lg mb-[20px] text-center" placeholder="Search Gif"
+       value={tag}
+       onChange={(event)=>setTag(event.target.value)}>
+      </input>
+
+      <button onClick={()=>fetchData(tag)} 
+              className="w-10/12 bg-blue-200  text-lg py-2 rounded-lg mb-[20px]">
+        Generate
+      </button>
+
+    </div>
+  );  
+}
+
+export default  Tag; 
